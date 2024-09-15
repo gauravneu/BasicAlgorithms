43c8949df5fa1f0d4868eeec22d32389e5a60fb1
gauravneu <99050945+gauravneu@users.noreply.github.com>
2024-09-15 14:26:10 -0400

    update

---
+++ BasicAlgorithmImpl/src/main/java/sortingAlgo/quickSortImpl/Quick.java
@@ -0,0 +1,16 @@
+package sortingAlgo.quickSortImpl;
+
+public class Quick {
+    private Quick(){}
+
+    public static void sort(Comparable[] a){
+
+    }
+
+
+    public static void main(String[] args) {
+        String[] a = new String[]{"ab","th","tg"};
+        Quick.sort(a);
+        show(a);
+    }
+}
